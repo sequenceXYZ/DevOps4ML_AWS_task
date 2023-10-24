4043df9d14e8aa6f224865ed2c9034957650a426
sequenceXYZ <122634843+sequenceXYZ@users.noreply.github.com>
2023-10-24 19:32:00 +0300

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -8,7 +8,15 @@ ls -l /var/log > /var/log/all_files.txt
 aws s3 cp /var/log/all_files.txt s3://agnija-bucket-devops4ml-aws/logs/all_files.txt
 
 # use the IMDSv2 method to retrieve the instance ID of the EC2 instance
-instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+# https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instancedata-data-retrieval.html
+TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` 
+instance_id=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/instance-id)
 
 # Terminate the EC2 instance
 aws ec2 terminate-instances --instance-ids $instance_id
+
+
+-------------------------------------------------------
+another option
+# use the IMDSv2 method to retrieve the instance ID of the EC2 instance
+instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
